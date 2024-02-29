7d48d1d8496c232e3e105219a36e326db08d5cea
Nitin Srivastava <nitin.srivastava1@appinventiv.com>
2024-03-01 00:51:20 +0530

    Added Camera capture heading

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ const App = () => {
     //   </Switch>
     // </Router>
     <>
+    <h2 style={{textAlign: "center"}}>Optical Character Reader</h2>
+    <h4 style={{textAlign: "center"}}>Choose file from below button and process image to get the recognized text</h4>
     <CameraCapture />
     </>
   );
